85a1d77de269fa2871163b9a6006615004ca2f30
TanziliaM <musatova.tanzilia@gmail.com>
2021-06-21 21:05:55 +0500

    V1

---
+++ src/main/java/ru/netology/domain/ButtonRepostObject.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class ButtonRepostObject {
+    private int idUser;
+    private boolean RepostButton;
+
+    // + get/set на все поля
+    }
+
+
---
+++ src/main/java/ru/netology/domain/PostVk.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class PostVk {
+    private int idPost;
+    private String imageUrl;
+    private String text;
+    private CommentsInfo commentsInfo;
+    private ButtonLikesObject buttonLikesObject;
+    private ButtonRepostObject buttonRepostObject;
+
+    // + get/set на все поля
+}
+
+
