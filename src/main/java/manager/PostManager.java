10e4ed7c2d3a5a745c5fb3adec38cebd0fcb8b12
TanziliaM <musatova.tanzilia@gmail.com>
2021-06-22 14:10:48 +0500

    hw2

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,31 @@
+package manager;
+
+import ru.netology.domain.PostVk;
+
+public class PostManager {
+    private PostVk[] posts;
+    public String generateBlock() {
+        // TODO: add logic
+        return null;
+    }
+
+    public PostVk[] search() {
+        int ownerId;
+        int domain;
+        String query;
+        boolean onlyOwners = true;
+        int maxCount = 100;
+        int minCount = 20;
+        public String generateBlock() {
+            // TODO: add logic
+            return null;
+        }
+    }
+        public void delete() {
+        boolean ownerId;
+        int postId;
+        public String generateBlock() {
+                // TODO: add logic
+        }
+    }
+}
